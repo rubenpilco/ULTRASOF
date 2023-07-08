a76bb57b0bbfaad3248dbe3a1c1a0410237fe324
rubenpilco <pilco_ruben3112@hotmail.es>
2023-07-07 20:17:59 -0500

    Add files via upload

---
+++ alumno.js
@@ -0,0 +1,13 @@
+[
+	{"alumno":"Ruben","nota":9.09,"ranking":10},
+	{"alumno":"Juan","nota":4,"ranking":7},
+	{"alumno":"Daniel","nota":8,"ranking":20},
+	{"alumno":"Maria","nota":10,"ranking":50},
+	{"alumno":"Pedro","nota":7.9,"ranking":15},
+	{"alumno":"Mayra","nota":5,"ranking":8,
+	{"alumno":"Roberto","nota":6,"ranking":4},
+	{"alumno":"Carlos","nota":3,"ranking":1},
+	{"alumno":"Luis","nota":7,"ranking":9},
+	{"alumno":"Paulina","nota":9,"ranking":18},
+	{"alumno":"Lorenzo","nota":8,"ranking":15}
+]
